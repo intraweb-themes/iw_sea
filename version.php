40a0964008de6789c62b167ea74f70e83972e8a2
fcasanellas <fcasanellas@e968f4be-1634-4921-b96b-e3dd0a78059a>
2010-10-27 09:37:39 +0000

    new SVN structure

---
+++ version.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * PostNuke Application Framework
+ *
+ * @copyright (c) 2006, Toni Ginard
+ * @link aginard@xtec.cat
+ * @version $Id: $
+ * @license GNU/GPL - http://www.gnu.org/copyleft/gpl.html
+ * @author Toni Ginard
+ * @package PostNuke_Themes
+ * @subpackage iw_xtec
+ */
+$dom = ZLanguage::getThemeDomain('theme_iw_sea'); 
+$themeversion['name'] = __('iw_sea', $dom);
+$themeversion['url'] = __('iw_sea', $dom);
+$themeversion['displayname'] = __('Intraweb sea', $dom);
+$themeversion['description'] = __('Theme developed by the Intraweb project staff', $dom);
+$themeversion['regid'] = 0;
+$themeversion['version'] = '1.0';
+$themeversion['official'] = 0;
+$themeversion['author'] = 'Toni Ginard';
+$themeversion['contact'] = 'toni.ginard@gmail.com';
+$themeversion['admin'] = 1;
+$themeversion['user'] = 1;
+$themeversion['system'] = 1;
+$themeversion['credits'] = '';
+$themeversion['help'] = '';
+$themeversion['changelog'] = '';
+$themeversion['license'] = 'GNU/GPL';
+$themeversion['xhtml'] = 1;
